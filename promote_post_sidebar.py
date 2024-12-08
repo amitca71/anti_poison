804af8576def3bf1ccdada1210df63daf3aa8599
amitca71 <amitca71@gmail.com>
2024-12-09 00:08:36 +0200

    change default prompt

--- promote_post_sidebar.py
+++ promote_post_sidebar.py
@@ -10,11 +10,16 @@ def promote_post_sidebar():
     # Streamlit app layout
         st.title("Promote post")
         with st.sidebar:
-            st.session_state["NUMBER_OF_SUGGESTIONS"] = st.slider('Number of suggestions', 1, 20, 7, step=1)
+            st.session_state["NUMBER_OF_SUGGESTIONS"] = st.slider('Number of suggestions', 1, 20, 10, step=1)
             st.session_state["TEMPERATURE"] = st.slider('Temperature - higher=more creative', 0.0, 1.0, 0.0, step=0.0001)
             number_of_responses=st.session_state["NUMBER_OF_SUGGESTIONS"]
-            st.session_state["SYSTEM_PROMPT"] = st.text_area("system prompt", f"""תן {number_of_responses} תגובות אפשריות שונות להדהוד הפוסט ברשתות חברתיות 
-            , ללא אימוג׳י. המשתמש יכול לתת לך הכוונה במה להתמקד בתשובה, בסוף הטקסט, אחרי המילים: ״הוראות מיוחדות״. אם הוא עושה זאת, כל התגובות חייבות להיות ברוח זו !!!!!!!!. התגובות ללא האשטאגים. מספר את התגובות, הפוסט הוא: """)
+#           st.session_state["SYSTEM_PROMPT"] = st.text_area("system prompt", f"""תן {number_of_responses} תגובות אפשריות שונות להדהוד הפוסט ברשתות חברתיות 
+#            , ללא אימוג׳י. המשתמש יכול לתת לך הכוונה במה להתמקד בתשובה, בסוף הטקסט, אחרי המילים: ״הוראות מיוחדות״. אם הוא עושה זאת, כל התגובות חייבות להיות ברוח זו !!!!!!!!. התגובות ללא האשטאגים. מספר את התגובות, הפוסט הוא: """)
+            st.session_state["SYSTEM_PROMPT"] = st.text_area("system prompt", f"""תן {number_of_responses} תגובות אפשריות לתמיכה בסרטון ברשתות חברתיות. 
+כל תגובה תהיה משפט או שני משפטים, החוזרים על נקודת מפתח בסרטון ויוצאים נגד הנושאים שנגדם יוצא הסרטון.
+המשפטים צריכים לבטא כעס. במידת האפשר השתמש באירוניה או בציניות.
+אל תשתמש בהאשטגים. לפני כל תגובה סמן #.
+הפוסט הוא:  """)
             st.session_state["TRENDING_WORDS"] = st.text_input("special instructions", "")
         # Optionally visualize graph data using third-party libraries
 
